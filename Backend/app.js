caea14038df4b9f59b6130e6cec845d258973cd8
Ankit <ankitsingh2000.15.1@gmail.com>
2024-08-03 13:47:15 +0530

    update origin

--- Backend/app.js
+++ Backend/app.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 app.use(cors({
-    origin: 'http://localhost:3000',  // Replace with your frontend domain
+    origin: 'http://localhost:3000/',  // Replace with your frontend domain
     credentials: true,  // Allow credentials to be sent
 }));
 
